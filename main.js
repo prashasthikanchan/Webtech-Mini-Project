dfb3fbafa45e655aaf0bbc08d64b7e2548822b99
unknown <prashasthikanchan2812@gmail.com>
2021-04-17 16:41:27 +0530

    signup page

--- main.js
+++ main.js
@@ -106,6 +106,16 @@ function slideFN() {
 
 /*end slidshow*/
 
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
 
 
